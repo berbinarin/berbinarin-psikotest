9666330403aff9539a21d9dc9742510d7241d222
elhamsyahrianputra <elhamsyahrianputra220402@gmail.com>
2025-08-16 05:30:56 +0700

    feat: delete barita user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,24 +27,5 @@ class UserSeeder extends Seeder
             'email' => 'ptpm@gmail.com',
             'password' => bcrypt('berbinar123')
         ])->assignRole('ptpm_psikotes-free');
-
-        $user = User::create([
-            'name' => 'Barita',
-            'username' => 'barita',
-            'email' => 'barita@gmail.com',
-            'password' => bcrypt('berbinar123')
-        ])->assignRole('user_psikotes-paid');
-
-        RegistrantProfile::create([
-            'user_id' => $user->id,
-            'test_type_id' => 1,
-            'gender' => 'female',
-            'age' => 20,
-            'domicile' => 'Bogor',
-            'phone_number' => '0123456789',
-            'psikotes_service' => 'offline',
-            'reason' => 'hanya uji coba',
-            'schedule' => '2025-10-25 14:20:00'
-        ]);
     }
 }
