58f09d05ca1ebcd01a8a82d931e4d395cc9afc5b
elhamsyahrianputra <elhamsyahrianputra220402@gmail.com>
2025-08-15 20:47:24 +0700

    fix: variable name and parameter

--- resources/views/dashboard/ptpm_psikotes-free/free-profiles/index.blade.php
+++ resources/views/dashboard/ptpm_psikotes-free/free-profiles/index.blade.php
@@ -11,13 +11,15 @@
             <div class="py-4 md:pb-7 md:pt-5">
                 <div>
                     <p tabindex="0" class="text-base font-bold leading-normal text-gray-800 focus:outline-none sm:text-lg md:text-2xl lg:text-3xl">Data Pendaftar Psikotes</p>
-                    <p class="text-gray-500 py-2">Fitur ini menampilkan informasi data pengguna yang telah melakukan registrasi Psikotes Berbinar</p>
-                    {{-- <a href="{{ route("dashboard.registrants.create") }}" class="mt-8 inline-flex items-start justify-start rounded bg-primary px-6 py-3 text-white hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-3">
+                    <p class="py-2 text-gray-500">Fitur ini menampilkan informasi data pengguna yang telah melakukan registrasi Psikotes Berbinar</p>
+                    {{--
+                        <a href="{{ route("dashboard.registrants.create") }}" class="mt-8 inline-flex items-start justify-start rounded bg-primary px-6 py-3 text-white hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-3">
                         <span class="leading-none">Tambah Data</span>
-                    </a> --}}
+                        </a>
+                    --}}
                 </div>
             </div>
-            <div class="rounded-[24px] bg-white px-10 py-7 mb-6 shadow">
+            <div class="mb-6 rounded-[24px] bg-white px-10 py-7 shadow">
                 <div class="mt-4 overflow-x-auto">
                     <table id="table" class="display gap-3" style="overflow-x: scroll">
                         <thead>
@@ -38,7 +40,7 @@
                                     <td class="text-center">{{ $loop->iteration }}</td>
                                     <td class="text-left">{{ $attempt->profile->name }}</td>
                                     <td class="text-left">{{ $attempt->profile->email }}</td>
-                                    <td class="text-center">{{ $attempt->gender === 'female' ? 'Perempuan' : 'Laki-Laki' }}</td>
+                                    <td class="text-center">{{ $attempt->gender === "female" ? "Perempuan" : "Laki-Laki" }}</td>
                                     <td class="text-center">
                                         {{ \Carbon\Carbon::parse($attempt->date_of_birth)->format("d-m-Y") }}
                                     </td>
@@ -49,13 +51,15 @@
                                         <a href="{{ route("dashboard.free-profiles.data.detail", $attempt->id) }}" class="inline-flex items-start justify-start rounded p-2 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2" style="background-color: #3b82f6">
                                             <i class="bx bx-show text-white"></i>
                                         </a>
-                                        {{-- <a href="{{ route("dashboard.attempts.edit", $attempt->id) }}" class="inline-flex items-start justify-start rounded p-2 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2" style="background-color: #e9b306">
+                                        {{--
+                                            <a href="{{ route("dashboard.attempts.edit", $attempt->id) }}" class="inline-flex items-start justify-start rounded p-2 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2" style="background-color: #e9b306">
                                             <i class="bx bx-edit-alt text-white"></i>
-                                        </a> --}}
-                                        <form id="deleteForm-{{ $freeProfile->id }}" action="{{ route("dashboard.free-profiles.destroy", $freeProfile->id) }}" method="POST">
+                                            </a>
+                                        --}}
+                                        <form id="deleteForm-{{ $attempt->id }}" action="{{ route("dashboard.free-profiles.destroy", $attempt->id) }}" method="POST">
                                             @csrf
                                             @method("DELETE")
-                                            <button type="button" class="delete-button inline-flex items-start justify-start rounded p-2 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2" style="background-color: #ef4444" data-id="{{ $freeProfile->id }}">
+                                            <button type="button" class="delete-button inline-flex items-start justify-start rounded p-2 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2" style="background-color: #ef4444" data-id="{{ $attempt->id }}">
                                                 <i class="bx bx-trash-alt text-white"></i>
                                             </button>
                                         </form>
