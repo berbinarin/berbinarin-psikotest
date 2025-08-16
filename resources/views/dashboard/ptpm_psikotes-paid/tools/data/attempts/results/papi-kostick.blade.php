06106006159b7ffb9bbd64a9b9f7d94c29c6a728
citrahlzh <citrahalijah1992@gmail.com>
2025-08-16 09:32:03 +0700

    fix: in progress

--- resources/views/dashboard/ptpm_psikotes-paid/tools/data/attempts/results/papi-kostick.blade.php
+++ resources/views/dashboard/ptpm_psikotes-paid/tools/data/attempts/results/papi-kostick.blade.php
@@ -17,7 +17,9 @@
                 <p class="select-text mb-1 text-[12px] font-semibold">Tanggal Pengerjaan</p>
                 <p class="select-text mb-1 text-[16px]">{{ \Carbon\Carbon::parse($attempt->created_at)->format("d F Y") }}</p>
                 <p class="select-text mb-1 text-[12px] font-semibold">Status</p>
-                <p>{{ $attempt->status ?? "Finished" }}</p>
+                <p>
+                    {{ $attempt->status === 'in_progress' ? 'Dalam Proses Pengerjaan' : ($attempt->status ?? 'Finished') }}
+                </p>
             </div>
         </div>
     </div>
