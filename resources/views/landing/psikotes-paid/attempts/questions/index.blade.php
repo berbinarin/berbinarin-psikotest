b1b11b86d2ba9af770696fe38bc2a6ec296ea5ba
citrahlzh <citrahalijah1992@gmail.com>
2025-09-13 16:36:02 +0700

    fix: essay

--- resources/views/landing/psikotes-paid/attempts/questions/index.blade.php
+++ resources/views/landing/psikotes-paid/attempts/questions/index.blade.php
@@ -143,11 +143,11 @@
             }
         });
 
-        const form = document.querySelector('form');
-        form.addEventListener('submit', function() {
-            // Hapus target-time dan section-order saat submit
-            localStorage.removeItem('target-time');
-            localStorage.removeItem('section-order');
-        });
+        // const form = document.querySelector('form');
+        // form.addEventListener('submit', function() {
+        //     // Hapus target-time dan section-order saat submit
+        //     localStorage.removeItem('target-time');
+        //     localStorage.removeItem('section-order');
+        // });
     </script>
 @endpush
