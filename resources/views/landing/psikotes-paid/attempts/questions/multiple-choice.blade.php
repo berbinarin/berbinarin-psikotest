1130c71f97919beeb6844e275c98fde1e38ad0ce
Verooo <yosuagerrad@gmail.com>
2026-02-03 14:14:35 +0800

    fix: option text pada multiple-choice

--- resources/views/landing/psikotes-paid/attempts/questions/multiple-choice.blade.php
+++ resources/views/landing/psikotes-paid/attempts/questions/multiple-choice.blade.php
@@ -10,8 +10,8 @@
             <label class="relative flex h-[62.67px] w-full cursor-pointer items-center">
                 <input type="radio" name="answer" value="{{ $option["key"] }}" class="peer absolute h-full w-full opacity-0" required />
                 <div class="flex h-full w-full items-center justify-start rounded-[13px] border-[1.33px] border-[#9E9E9E] font-plusJakartaSans text-[13.33px] font-semibold text-black transition-colors duration-200 peer-checked:bg-[#3986A3] peer-checked:text-white">
-                    <div class="ml-4 mr-4 h-5 w-5 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
-                    {{ $option["text"] }}
+                    <div class="ml-4 mr-4 h-5 w-5 flex-shrink-0 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
+                    <span class="flex-1 whitespace-normal px-4">{{ $option["text"] }}</span>
                 </div>
             </label>
         @endforeach
@@ -22,8 +22,8 @@
             <label class="relative flex h-[62.67px] w-full cursor-pointer items-center">
                 <input type="radio" name="answer" value="{{ $option["key"] }}" class="peer absolute h-full w-full opacity-0" required />
                 <div class="flex h-full w-full items-center justify-start rounded-[13px] border-[1.33px] border-[#9E9E9E] font-plusJakartaSans text-[13.33px] font-semibold text-black transition-colors duration-200 peer-checked:bg-[#3986A3] peer-checked:text-white">
-                    <div class="ml-4 mr-4 h-5 w-5 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
-                    {{ $option["text"] }}
+                    <div class="ml-4 mr-4 h-5 w-5 flex-shrink-0 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
+                    <span class="flex-1 whitespace-normal px-4">{{ $option["text"] }}</span>
                 </div>
             </label>
         @endforeach
@@ -33,8 +33,8 @@
         <label class="relative flex h-[62.67px] w-[520.33px] cursor-pointer items-center">
             <input type="radio" name="answer" value="{{ $option["key"] }}" class="peer absolute h-full w-full opacity-0" required />
             <div class="flex h-full w-full items-center justify-start rounded-[13px] border-[1.33px] border-[#9E9E9E] font-plusJakartaSans text-[13.33px] font-semibold text-black transition-colors duration-200 peer-checked:bg-[#3986A3] peer-checked:text-white">
-                <div class="ml-4 mr-4 h-5 w-5 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
-                {{ $option["text"] }}
+                <div class="ml-4 mr-4 h-5 w-5 flex-shrink-0 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
+                <span class="flex-1 whitespace-normal px-4">{{ $option["text"] }}</span>
             </div>
         </label>
     @endforeach
