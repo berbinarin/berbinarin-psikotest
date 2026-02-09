f107bd516301aff90995e4881d352bb5654fa42e
Verooo <yosuagerrad@gmail.com>
2026-02-09 11:42:17 +0800

    fix: tampilan bug pada brwoser safari

--- resources/views/landing/psikotes-paid/attempts/questions/multiple-choice.blade.php
+++ resources/views/landing/psikotes-paid/attempts/questions/multiple-choice.blade.php
@@ -7,11 +7,13 @@
 @if (count($question->options) >= 5)
     <div class="mb-4 mt-12 grid grid-cols-3 gap-4">
         @foreach ($question->options->slice(0, 3) as $option)
-            <label class="relative flex h-[62.67px] w-full cursor-pointer items-center">
+            <label class="relative flex h-[62px] w-full cursor-pointer items-center">
                 <input type="radio" name="answer" value="{{ $option["key"] }}" class="peer absolute h-full w-full opacity-0" required />
                 <div class="flex h-full w-full items-center justify-start rounded-[13px] border-[1.33px] border-[#9E9E9E] font-plusJakartaSans text-[13.33px] font-semibold text-black transition-colors duration-200 peer-checked:bg-[#3986A3] peer-checked:text-white">
                     <div class="ml-4 mr-4 h-5 w-5 flex-shrink-0 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
-                    <span class="flex-1 whitespace-normal px-4">{{ $option["text"] }}</span>
+                    <span class="min-w-0 flex-1 break-words px-4 text-left">
+                        {{ $option["text"] }}
+                    </span>
                 </div>
             </label>
         @endforeach
@@ -19,22 +21,26 @@
 
     <div class="grid grid-cols-2 gap-4">
         @foreach ($question->options->slice(3, 2) as $option)
-            <label class="relative flex h-[62.67px] w-full cursor-pointer items-center">
+            <label class="relative flex h-[62px] w-full cursor-pointer items-center">
                 <input type="radio" name="answer" value="{{ $option["key"] }}" class="peer absolute h-full w-full opacity-0" required />
                 <div class="flex h-full w-full items-center justify-start rounded-[13px] border-[1.33px] border-[#9E9E9E] font-plusJakartaSans text-[13.33px] font-semibold text-black transition-colors duration-200 peer-checked:bg-[#3986A3] peer-checked:text-white">
                     <div class="ml-4 mr-4 h-5 w-5 flex-shrink-0 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
-                    <span class="flex-1 whitespace-normal px-4">{{ $option["text"] }}</span>
+                    <span class="min-w-0 flex-1 break-words px-4 text-left">
+                        {{ $option["text"] }}
+                    </span>
                 </div>
             </label>
         @endforeach
     </div>
 @else
     @foreach ($question->options as $option)
-        <label class="relative flex h-[62.67px] w-[520.33px] cursor-pointer items-center">
+        <label class="relative flex h-[62px] w-[520px] cursor-pointer items-center">
             <input type="radio" name="answer" value="{{ $option["key"] }}" class="peer absolute h-full w-full opacity-0" required />
             <div class="flex h-full w-full items-center justify-start rounded-[13px] border-[1.33px] border-[#9E9E9E] font-plusJakartaSans text-[13.33px] font-semibold text-black transition-colors duration-200 peer-checked:bg-[#3986A3] peer-checked:text-white">
                 <div class="ml-4 mr-4 h-5 w-5 flex-shrink-0 rounded-full border-2 border-[#9E9E9E] bg-white peer-checked:border-4 peer-checked:border-white peer-checked:bg-[#3986A3]"></div>
-                <span class="flex-1 whitespace-normal px-4">{{ $option["text"] }}</span>
+                <span class="min-w-0 flex-1 break-words px-4 text-left">
+                    {{ $option["text"] }}
+                </span>
             </div>
         </label>
     @endforeach
