3bc916bff34ae84540fb8ec5ae874759868c8db7
alfinizz12 <alfinizzudien12@gmail.com>
2026-02-10 19:11:05 +0700

    chore: add .DS_Store to gitignore, feat: MSDT report tools integration

--- resources/views/dashboard/ptpm_psikotes-paid/registrants/report/tools/msdt.blade.php
+++ resources/views/dashboard/ptpm_psikotes-paid/registrants/report/tools/msdt.blade.php
@@ -114,50 +114,40 @@
     </head>
 
     <body>
-        <h2 class="section-title">Hasil Tes Psikologi</h2>
+        <h2 class="section-title">Hasil Tes Psikologi MSDT</h2>
         <div class="info-section">
             <p>
                 <span>Kategori:</span>
-                <span class="info-value">Bureaucrat</span>
+                <span class="info-value">{{ $data[0]['description']['type'] }}</span>
             </p>
             <p>
                 <span>Status:</span>
-                <span class="info-value">Lebih Efektif</span>
+                <span class="info-value">{{ $data[0]['description']['status'] }}</span>
             </p>
         </div>
 
         {{-- Rincian Jawaban --}}
         <div class="content-section">
             <h3>Rincian Jawaban:</h3>
-            <p class="content-text">Ditampilkan oleh pemimpin yang menggunakan TO rendah RO rendah. Dalam situasi yang sesuai sehingga ia dapat bertindak efektif ia merupakan pemimpin yang menekankan pada aturan dan prosedur untuk kepentingan dirinya serta menjalankan pengaturan dan pengawasan dengan caranya sendiri. Ia termasuk pemimpin yang hati-hati dalam bertindak.</p>
-            <p class="content-text">Pemimpin yang bertipe bureaucrat adalah tipe pemimpin yang hanya berorientasi pada keefektifan saja. Pemimpin tipe ini memiliki sifat-sifat sebagai berikut:</p>
+            @foreach ($data[0]['description']['text']['description'] ?? [] as $desc)
+                @if(!($loop->last))
+                    <p>{{ $desc }}</p>
+                @else
+                    <p>{!! $desc !!}. Pemimpin tipe ini memiliki sifat-sifat sebagai berikut:</p>
+                @endif
+            @endforeach
 
             <div class="content-list">
-                <div class="content-list-item">
-                    <p class="content-list-title">1) Selalu berorientasi pada keefektifan. Hal ini tampak pada sifat-sifatnya sebagai berikut:</p>
-                    <ul>
-                        <li>Bekerja sesuai dengan prosedur yang benar dan peraturan yang berlaku.</li>
-                        <li>Taat pada peraturan organisasi serta taat pada perintah secara tepat.</li>
-                        <li>Memelihara kepentingan lingkungan dengan selalu mengikuti aturan-aturan manajerial.</li>
-                    </ul>
-                </div>
-
-                <div class="content-list-item">
-                    <p class="content-list-title">2) Tidak atau kurang berorientasi pada tugas. Hal ini tampak pada sifat-sifatnya antara lain:</p>
-                    <ul>
-                        <li>Tidak menyukai tugas yang diserahkan kepadanya.</li>
-                        <li>Ide-idenya tidak atau kurang mendorong untuk meningkatkan produksi.</li>
-                    </ul>
-                </div>
-
-                <div class="content-list-item">
-                    <p class="content-list-title">3) Tidak atau kurang berorientasi pada hubungan dengan orang lain. Hal ini tampak pada sifat-sifatnya antara lain:</p>
-                    <ul>
-                        <li>Tidak atau kurang menyukai masyarakat.</li>
-                        <li>Tidak atau kurang mengembangkan hubungan dengan bawahannya.</li>
-                        <li>Sangat percaya bahwa hubungan baik dengan orang lain adalah sulit untuk dicapai.</li>
-                    </ul>
-                </div>
+                @foreach ($data[0]['description']['text']['feature'] ?? [] as $feature)
+                    <div class="content-list-item">
+                        <p class="content-list-title">{{ $loop->iteration }}) {{ $feature['general'] }}. Hal ini tampak pada sifat-sifatnya sebagai berikut:</p>
+                        <ul>
+                            @foreach ($feature['characteristic'] ?? [] as $characteristic)
+                                <li>{{ $characteristic }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endforeach
             </div>
         </div>
 
@@ -173,24 +163,24 @@
                     </tr>
                 </thead>
             </table>
-            @for ($i = 1; $i <= 8; $i++)
+            @foreach($attempt->responses->sortBy('question.order') as $response)
                 <table class="detail-table">
                     <tr>
-                        <td rowspan="2" class="center-text">{{ $i }}.</td>
+                        <td rowspan="2" class="center-text">{{ $loop->iteration }}.</td>
                         <td>
                             <span>A.</span>
-                            Saya tidak akan menegur pelanggar-pelanggar peraturan bila saya merasa pasti bahwa tidak ada satu orang pun yang mengetahui tentang pelanggar-pelanggar tersebut.
+                            {{ $response->question['options'][0]['text'] }}
                         </td>
-                        <td rowspan="2" class="center-text" style="font-weight: bold">A</td>
+                        <td rowspan="2" class="center-text" style="font-weight: bold">{{ collect($response->question['options'])->firstWhere('key', $response->answer['choice'])['key'] ?? '-' }}</td>
                     </tr>
                     <tr>
                         <td>
                             <span>B.</span>
-                            Bila saya mengumumkan suatu keputusan yang kurang menyenangkan, saya akan menjelaskan kepada bawahan saya bahwa keputusan ini dibuat oleh direktur.
+                            {{ $response->question['options'][1]['text'] }}
                         </td>
                     </tr>
                 </table>
-            @endfor
+            @endforeach
         </div>
     </body>
 </html>
